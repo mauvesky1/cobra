26380ddfefd51cf221193323088103fbd84efe34
RobertDraper <rob.draper@live.com>
2024-06-01 19:47:20 +0100

    Phone component created and added to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Phone from "@/components/Phone";
 import { Check, Star } from "lucide-react";
 
 
@@ -13,7 +14,7 @@ export default function Home() {
                   <img src="/snake-1.png" className="w-fulls" />
                 </div>
                 <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span> Phone Case</h1>
-                <p className = "mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap" > Capture your favourite memories with your own <span className="font-semibold">one-of-a-kind</span>phone case. CaseCobra allows you to make your phone stand out, not just protect your phone.</p>
+                <p className = "mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap" > Capture your favourite memories with your own <span className="font-semibold">one-of-a-kind </span>phone case. CaseCobra doesn't just provide protection, it also allows you to make your phone stand oute.</p>
                 <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
                   <div className="space-y-2">
                     <li className="flex gap-1.5 items-center text-left">
@@ -26,7 +27,7 @@ export default function Home() {
                     </li>
                     <li className="flex gap-1.5 items-center text-left">
                       <Check className="h-5 w-5 shrink-0 text-green-600" />
-                      Modern iphone models supported
+                      Latest iphone models supported
                     </li>
                     </div>                   
                 </ul>
@@ -51,7 +52,13 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="col-span-ful lg:col-span-1 w-full flex justify-center pex-8 sm:px-16 md:px"></div>
+            <div className="col-span-ful lg:col-span-1 w-full flex justify-center pex-8 sm:px-16 md:px-0 mt:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+                    <div className="relative md:max-w-xl">
+                      <img src="/your-image.png" className="absolute w-40 lg:w-52 left-56 -top-20 select-none  sm:block lg:hidden xl:block" />
+                      <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" />
+                      <Phone imgSrc="/testimonials/1.jpg"  className="w-64"/>
+                    </div>
+            </div>
           </MaxWidthWrapper>
         </section>
     </div>

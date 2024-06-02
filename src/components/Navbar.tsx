ca312ab622c5238001c62451d2aa6702942f9376
RobertDraper <rob.draper@live.com>
2024-06-02 12:05:28 +0100

    Navbar:Register and Login buttons added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,8 +35,28 @@ const Navbar = () => {
                         </Link> 
                         </>
   ) :(
-    <></>
-  )   }     
+            <>
+            <Link href="/api/auth/register" className={buttonVariants({
+                size: "sm",
+                variant: "ghost",
+            })}>Sign Up</Link>    
+                    
+            <Link href="/apit/auth/login" className={buttonVariants({
+                size: "sm",
+                variant: "ghost"
+            })}>Log In 
+                </Link> 
+                <div className="h-8 w-px bg-zinc-200 hidden sm:block">
+                    
+                </div>
+                </>
+        )
+        }  
+            <Link href="/configure/upload" className={buttonVariants({
+                size: "sm",
+                className: "hidden sm:flex items-center gap-1",
+            })}>Create case <ArrowRight className="ml-1.5 h-5 w-5" />
+            </Link>    
             </div>
             </div>
         </MaxWidthWrapper>

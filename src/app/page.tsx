08ca21fbf31575704243c504a65b28eae8ac2320
RobertDraper <rob.draper@live.com>
2024-06-02 10:48:34 +0100

    Navbar component created and added to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
             </div>
             <div className="col-span-ful lg:col-span-1 w-full flex justify-center pex-8 sm:px-16 md:px-0 mt:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
                     <div className="relative md:max-w-xl">
-                      <img src="/your-image.png" className="absolute w-40 lg:w-52 left-56 -top-20 select-none  sm:block lg:hidden xl:block" />
+                      <img src="/your-image.png" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" />
                       <img src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" />
                       <Phone imgSrc="/testimonials/1.jpg"  className="w-64"/>
                     </div>
